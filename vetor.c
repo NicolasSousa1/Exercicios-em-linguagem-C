0c8ac196154aceb31701971eb5bc79a755ff04ac
Nicolas de Sousa <dsousa.nicolas@gmail.com>
2026-09-08 12:59:05 -0300

    exercício para treinar funções

---
+++ vetor.c
@@ -0,0 +1,48 @@
+/* Programa para carregar um  vetor
+    Nicolas de Sousa
+    08/09/26
+*/
+
+#include <stdio.h>
+
+int carregarVetor();
+
+int calcularMedia(int *);
+
+void exibirMediaArredondada(int);
+
+main(){
+
+    
+    exibirMediaArredondada(carregarVetor());
+
+}
+
+int carregarVetor(){
+
+    int vetor[10];
+
+    for(int i = 0; i < 10; i++){
+
+        printf("Digite o numero da posicao %d: ", i+1);
+        scanf("%d", &vetor[i]);
+
+    }
+
+    return calcularMedia(vetor);
+    
+}
+
+int calcularMedia(int *vetor){
+    int media = 0;
+    for(int i = 0; i < 10; i++){
+        media += vetor[i];
+    }
+    media = media / 10;
+
+    return media;
+}
+
+void exibirMediaArredondada(int media){
+    printf("A media arredondada e: %d", media);
+}
